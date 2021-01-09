63d918a9574445e49f4deea8b762ff8329f1b8bc
Vorafas <40887473+Vorafas@users.noreply.github.com>
2021-01-09 23:50:49 +0500

    Updated main.py

--- lab-1/taskOne/main.py
+++ lab-1/taskOne/main.py
@@ -1,23 +1,20 @@
+def getNumber(text):
+    res: int
+    while True:
+        try:
+            res = int(input(text))
+            break
+        except ValueError:
+            print("Введено не число.")
+    return res
+
 radius = 1.5
 
 xCenter = 3
 yCenter = 3
 
-x: int
-while True:
-    try:
-        x = float(input("Веедите значение x: "))
-        break
-    except ValueError:
-        print("Введено не число.")
-
-y: int
-while True:
-    try:
-        y = float(input("Веедите значение y: "))
-        break
-    except ValueError:
-         print("Введено не число.")
+x = getNumber("Веедите значение x: ")
+y = getNumber("Веедите значение y: ")
 
 result = (x - xCenter) ** 2 + (y - yCenter) ** 2 <= radius ** 2
 
